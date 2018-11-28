646a8b890d0e8778160cf23958275c9f25c42db7
smurf <smurf>
2018-11-28 18:28:10 +0000

    query 3 e query 4 feitas

--- queries.sql
+++ queries.sql
@@ -27,20 +27,21 @@
   	WHERE cnt >= 2 ) as numTelFilt
   	NATURAL JOIN EventoEmergencia
 
-
-
 -- Query 3
   -- Quais sao os processos de socorro, referente a eventos de emergencia em 2018
   -- de Oliveira do Hospital, onde existe pelo menos um acionamento de meios que
   -- não foi alvo de auditoria;
 
+SELECT numProcessoSocorro FROM (
+  SELECT numProcessoSocorro FROM eventoEmergencia NATURAL JOIN acciona WHERE moradaLocal='Oliveira do Hospital' AND instanteChamada BETWEEN '2018-01-01 00:00:00' AND '2018-12-31 23:59:59'
+) as evento
+  EXCEPT(SELECT numProcessoSocorro FROM audita);
 
 -- Query 4
   -- Quantos segmentos de vídeo com duração superior a 60 segundos, foram gravados
   -- em câmeras de vigilância de Monchique durante o mês de Agosto de 2018;
 
-(SELECT COUNT(*) AS segmentos_video FROM segmentoVideo NATURAL JOIN video NATURAL JOIN vigia WHERE moradaLocal='Monchique' AND dataHoraInicio >='2018-08-01 00:00:00' AND dataHoraFim<='2018-08-31 23:59:59' AND duracao='00:01:00');
-
+SELECT COUNT(*) AS segmentos_video FROM segmentoVideo NATURAL JOIN video NATURAL JOIN vigia WHERE moradaLocal='Monchique' AND dataHoraInicio >='2018-08-01 00:00:00' AND dataHoraFim<='2018-08-31 23:59:59' AND duracao='00:01:00';
 
 -- Query 5
   -- Liste os Meios de combate que não foram usados como Meios de Apoio em nenhum
