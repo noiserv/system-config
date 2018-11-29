fb7b97ff8f7df9e05bd1ec74312bb70a89c312e3
smurf <smurf>
2018-11-29 17:25:51 +0000

    upgrade da interface em termos esteticos

--- web/a1.php
+++ web/a1.php
@@ -4,13 +4,46 @@
     <meta http-equiv="content-type" content="text/html" charset="utf-8"/>
   </head>
   <body>
+  <div style='position:fixed;left:30px;top:50px;'>
+  <a href='a.html'><button class='btn btn-dark' style='background: #000000 !important;color: #ffffff' type='button'>Voltar</button></a><br><br>
+    <form action='a1.php' method='post'>
+      <h3>Nova Zona:<br></h3>
+      <input type='hidden' name='mode' value='add'/>
+      <input type='hidden' name='type' value='zona'/>
+      <input type='text' name='morada' placeholder='Morada'/><br><br>
+      <button class='btn btn-info' type='submit' value='submit'>Submit</button>
+    </form><br>
+    <form action='a1.php' method='post'>
+      <h3>Novo Evento de Emergência:<br></h3>
+      <input type='hidden' name='mode' value='add'/>
+      <input type='hidden' name='type' value='eventoEmergencia'/>
+      <input type='text' name='nome'placeholder='Nome'/><br><br>
+      <input type='text' name='morada' placeholder='Morada'/><br><br>
+      <input type='number' name='numprocesso' placeholder='N&uacute;mero de processo'/><br><br>
+      <input type='text' name='telefone' placeholder='Telefone'/><br><br>
+      <input type='text' name='chamada' placeholder='2018-11-18 10:00:00'/><br><br>
+      <button class='btn btn-info' type='submit' value='submit'>Submit</button>
+    </form><br>
+      <form action='a1.php' method='post'>
+      <h3>Novo Processo de Socorro:<br></h3>
+      <input type='hidden' name='mode' value='add'/>
+      <input type='hidden' name='type' value='processoSocorro'/>
+      <input type='number' name='numprocesso' placeholder='N&uacute;mero de processo'/><br><br>
+      <button class='btn btn-info' type='submit' value='submit'>Submit</button><br><br>
+    </form>
+  </div>
 
 <?php
   try  {
     include 'config.php';
     /* ADICIONA*/
     if ($mode == "add") {
-      if($type == "zona" or $type == "eventoEmergencia"){
+      if($type=='zona'){
+        $result = $db->prepare("INSERT INTO zona VALUES(:moradaLocal);");
+        $result->bindParam(':moradaLocal', $_REQUEST['morada']);
+        $result->execute();
+      }
+      elseif($type == "eventoEmergencia"){
         $result = $db->prepare("SELECT COUNT(*) AS total FROM zona WHERE moradaLocal = :morada;");
         $result->bindParam(':morada', $_REQUEST['morada']);
         $result->execute();  
@@ -21,8 +54,6 @@
             $result->execute();
           }
         }
-      }
-      if($type == "processoSocorro" or $type == "eventoEmergencia"){
         $result = $db->prepare("SELECT COUNT(*) AS total FROM processoSocorro WHERE numProcessoSocorro = :numprocesso;");
         $result->bindParam(':numprocesso', $_REQUEST['numprocesso']);
         $result->execute();  
@@ -32,9 +63,7 @@
             $result->bindParam(':numProcessoSocorro', $_REQUEST['numprocesso']);
             $result->execute();
           }
-        }
-      }
-      if($type == "eventoEmergencia"){     
+        } 
         $result = $db->prepare("INSERT INTO eventoEmergencia VALUES(:nomePessoa, :moradaLocal, :numProcessoSocorro, :numTelefone, :instanteChamada);");
         $result->bindParam(':nomePessoa', $_REQUEST['nome']);
         $result->bindParam(':moradaLocal', $_REQUEST['morada']);
@@ -42,10 +71,14 @@
         $result->bindParam(':numTelefone', $_REQUEST['telefone']);
         $result->bindParam(':instanteChamada', $_REQUEST['chamada']);
         $result->execute();
+      }elseif($type=='processoSocorro'){
+        $result = $db->prepare("INSERT INTO processoSocorro VALUES(:numProcessoSocorro);");
+        $result->bindParam(':numProcessoSocorro', $_REQUEST['numprocesso']);
+        $result->execute();
       }
    }
     /* APAGA*/
-    if ($mode == "delete") {
+    elseif ($mode == "delete") {
       if ($type == "zona") {
         $prep = $db->prepare("DELETE FROM zona WHERE moradaLocal = :morada;");
         $prep->bindParam(':morada', $_REQUEST['morada']);
@@ -68,28 +101,21 @@
       }
     }
   }
-    echo("<a href='a.html' style='position:fixed;left:30px;top:50px'><button class='btn btn-dark' style='background: #000000 !important;color: #ffffff' type='button'>Voltar</button></a><br><br>");
-    /* ZONA*/
+    /* ZONA */
     $prep = $db->prepare("SELECT moradaLocal FROM zona;");
     $prep->execute();
     $result = $prep->fetchAll();
 
-    echo("<br><br><h1 align='center'><strong>Zonas</h1>\n");
-    echo("<table border=\"2\" border-style= 'double' align='center'>");
+    echo("<div style='margin-left:50px'><br><br><h1 align='center'><strong>Zonas</h1>\n");
+    echo("<table border=\"2\" align='center'>");
     echo("<tr><td><b><strong>Moradas:</b></td><td></td></tr>");
 
     foreach($result as $row) {
       echo("<tr><td align='center'>");
       echo($row['moradalocal']);
-      echo("</td><td><a href=\"a1.php?mode=delete&type=zona&morada={$row['moradalocal']}\">delete</a></td></tr>\n");
+      echo("</td><td><a href=\"a1.php?mode=delete&type=zona&morada={$row['moradalocal']}\">Delete</a></td></tr>\n");
     }
     echo("</table><br><br>");
-    echo("<form align='center' action='a1.php' method='post'>");
-    echo("<input type='hidden' name='mode' value='add'/>");
-    echo("<input type='hidden' name='type' value='zona'/>");
-    echo("<input type='text' name='morada' placeholder='Morada'/><br><br>");
-    echo("<button class='btn btn-info' type='submit' value='submit'>Submit</button>");
-    echo("</form><br><br>");
 
     /* EVENTO EMERGENCIA*/
     $prep = $db->prepare("SELECT moradaLocal, numProcessoSocorro, nomePessoa, numTelefone, instanteChamada FROM eventoEmergencia;");
@@ -97,7 +123,7 @@
     $result = $prep->fetchAll();
 
     echo("<h1 align='center'><strong>Eventos de Emerg&ecirc;ncia</h1>\n");
-    echo("<table border=\"2\" border-style= 'double' align='center'>");
+    echo("<table border=\"2\" align='center'>");
     echo("<tr><td><b><strong>N&uacute;mero de Processo:</b></td><td><b><strong>Moradas:</b></td><td><b><strong>Nome da Pessoa:</b></td><td><b><strong>N&uacute;mero de Telefone:</b></td><td><b><strong>Instante Chamada:</b></td><td></td></tr>\n");
 
     foreach($result as $row)
@@ -112,19 +138,9 @@
         echo($row['numtelefone']);
         echo("</td><td align='center'>");
         echo($row['instantechamada']);
-        echo("</td><td><a href=\"a1.php?mode=delete&type=eventoEmergencia&numprocesso={$row['numprocessosocorro']}&morada={$row['moradalocal']}&nome={$row['nomepessoa']}&telefone={$row['numtelefone']}&chamada={$row['instantechamada']}\">delete</a></td></tr>\n");
+        echo("</td><td><a href=\"a1.php?mode=delete&type=eventoEmergencia&numprocesso={$row['numprocessosocorro']}&morada={$row['moradalocal']}&nome={$row['nomepessoa']}&telefone={$row['numtelefone']}&chamada={$row['instantechamada']}\">Delete</a></td></tr>\n");
     }
     echo("</table><br><br>");
-    echo("<form align='center' action='a1.php' method='post'>");
-    echo("<input type='hidden' name='mode' value='add'/>");
-    echo("<input type='hidden' name='type' value='eventoEmergencia'/>");
-    echo("<input type='text' name='nome'placeholder='Nome'/><br><br>");
-    echo("<input type='text' name='morada' placeholder='Morada'/><br><br>");
-    echo("<input type='number' name='numprocesso' placeholder='N&uacute;mero de processo'/><br><br>");
-    echo("<input type='text' name='telefone' placeholder='Telefone'/><br><br>");
-    echo("<input type='text' name='chamada' placeholder='2018-11-18 10:00:00'/><br><br>");
-    echo("<button class='btn btn-info' type='submit' value='submit'>Submit</button>");
-    echo("</form>");
 
     /* PROCESSO SOCORRO */
     $prep = $db->prepare("SELECT numProcessoSocorro FROM processoSocorro;");
@@ -133,27 +149,22 @@
 
     echo("<br><br>");
     echo("<h1 align='center'><strong>Processos de Socorro</h1>\n");
-    echo("<table border=\"2\" border-style= 'double' align='center'>");
+    echo("<table border=\"2\" align='center'>");
     echo("<tr><td><b><strong>Números:</b></td><td></td></tr>");
 
     foreach($result as $row)
     {
       echo("<tr><td align='center'>");
       echo($row['numprocessosocorro']);
-      echo("</td><td><a href=\"a1.php?mode=delete&type=processoSocorro&numprocesso={$row['numprocessosocorro']}\">delete</a></td></tr>\n");
+      echo("</td><td><a href=\"a1.php?mode=delete&type=processoSocorro&numprocesso={$row['numprocessosocorro']}\">Delete</a></td></tr>\n");
     }
-    echo("</table><br><br>");
-    echo("<form align='center' action='a1.php' method='post'>");
-    echo("<input type='hidden' name='mode' value='add'/>");
-    echo("<input type='hidden' name='type' value='processoSocorro'/>");
-    echo("<input type='number' name='numprocesso' placeholder='N&uacute;mero de processo'/><br><br>");
-    echo("<button class='btn btn-info' type='submit' value='submit'>Submit</button><br><br>");
-    echo("</form><br><br>");
+    echo("</table></div><br><br>");
 
     $result = null;
     $db = null;
 } catch (PDOException $e) {
-  echo("<p>ERROR: {$e->getMessage()}</p>");
+  echo("<div align='center'><br><p>ERROR: {$e->getMessage()}</p>");
+  echo("<a href='a1.php'><button class='btn btn-dark' style='background: #000000 !important;color: #ffffff' type='button'>Tente outra vez</button></a></div>");
 }
 ?>
   </body>
--- web/a2.php
+++ web/a2.php
@@ -5,30 +5,31 @@
   </head>
   <body>
 
+  <div style='position:fixed;left:30px;top:50px'>
+  <a href='a.html'><button class='btn btn-dark' style='background: #000000 !important;color: #ffffff' type='button'>Voltar</button></a><br><br>
+    <br><br><form action='a2.php' method='post'>
+      <h3>Novo Meio:<br></h3>
+      <input type='hidden' name='mode' value='add'/>
+      <input type='hidden' name='type' value='meio'/>
+      <input type='number' name='nummeio' placeholder='N&uacute;mero meio'/><br><br>
+      <input type='text' name='nomemeio' placeholder='Nome meio'/><br><br>
+      <input type='text' name='nomeentidade' placeholder='Nome entidade'/><br><br>
+      <button class='btn btn-info' type='submit' value='submit'>Sumit</button>
+    </form><br><br>
+    <form action='a2.php' method='post'>
+      <h3>Nova Entidade:<br></h3>
+      <input type='hidden' name='mode' value='add'/>
+      <input type='hidden' name='type' value='entidade'/>
+      <input type='text' name='nomeentidade' placeholder='Nome entidade'/><br><br>
+      <button class='btn btn-info' type='submit' value='submit'>Sumit</button>
+    </form>
+  </div>
 <?php
   try  {
     include 'config.php';
     /* ADICIONA*/
     if ($mode == "add") {
-      if ($type == "meio" or $type == "entidade") {
-        $result = $db->prepare("SELECT COUNT(*) AS total FROM entidadeMeio WHERE nomeEntidade = :nomeentidade;");
-        $result->bindParam(':nomeentidade', $_REQUEST['nomeentidade']);
-        $result->execute();  
-        foreach($result as $row){
-          if($row['total']<1){
-            $result = $db->prepare("INSERT INTO entidadeMeio VALUES(:nomeEntidade);");
-            $result->bindParam(':nomeEntidade', $_REQUEST['nomeentidade']);
-            $result->execute();
-          }
-        }
-        if($type == "meio"){
-          $result = $db->prepare("INSERT INTO meio VALUES(:numMeio, :nomeMeio, :nomeEntidade);");
-          $result->bindParam(':numMeio', $_REQUEST['nummeio']);
-          $result->bindParam(':nomeMeio', $_REQUEST['nomemeio']);
-          $result->bindParam(':nomeEntidade', $_REQUEST['nomeentidade']);
-          $result->execute();
-        }
-      }
+       include 'meio.php';
     }
     /* APAGA*/
     if ($mode == "delete") {
@@ -50,7 +51,6 @@
       }
     }
     
-    echo("<a href='a.html' style='position:fixed;left:30px;top:50px'><button class='btn btn-dark' style='background: #000000 !important;color: #ffffff' type='button'>Voltar</button></a><br><br>");
     /* MEIO */
     $prep = $db->prepare("SELECT numMeio, nomeMeio, nomeEntidade FROM meio;");
     $prep->execute();
@@ -58,7 +58,7 @@
 
     echo("<br><br>");
     echo("<h1 align='center'><strong>Meios</h1>");
-    echo("<table border=\"2\" border-style= 'double' align='center'>");
+    echo("<table border=\"2\" align='center'>");
     echo("<tr><td align='center'><b><strong>N&uacute;mero dos Meios:</b></td><td align='center'><b><strong>Nome dos Meios:</b></td><td align='center'><b><strong>Nome das entidades:</b></td><td></td></tr>");
 
     foreach($result as $row)
@@ -69,17 +69,9 @@
         echo($row['nomemeio']);
         echo("</td><td align='center'>");
         echo($row['nomeentidade']);
-        echo("</td><td><a href=\"a2.php?mode=delete&type=meio&nummeio={$row['nummeio']}&nomemeio={$row['nomemeio']}&nomeentidade={$row['nomeentidade']}\">delete</a></td></tr>\n");
+        echo("</td><td><a href=\"a2.php?mode=delete&type=meio&nummeio={$row['nummeio']}&nomemeio={$row['nomemeio']}&nomeentidade={$row['nomeentidade']}\">Delete</a></td></tr>\n");
     }
     echo("</table><br><br>");
-    echo("<form align='center' action='a2.php' method='post'>");
-    echo("<input type='hidden' name='mode' value='add'/>");
-    echo("<input type='hidden' name='type' value='meio'/>");
-    echo("<input type='number' name='nummeio' placeholder='N&uacute;mero meio'/><br><br>");
-    echo("<input type='text' name='nomemeio' placeholder='Nome meio'/><br><br>");
-    echo("<input type='text' name='nomeentidade' placeholder='Nome entidade'/><br><br>");
-    echo("<button class='btn btn-info' type='submit' value='submit'>Sumit</button>");
-    echo("</form>");
 
     /* ENTIDADE MEIO*/
     $prep = $db->prepare("SELECT nomeEntidade FROM entidadeMeio;");
@@ -88,27 +80,22 @@
 
     echo("<br><br>");
     echo("<h1 align='center'><strong>Entidades</h1>");
-    echo("<table border=\"2\" border-style= 'double' align='center'>");
+    echo("<table border=\"2\" align='center'>");
     echo("<tr><td><b><strong>Nomes:</b></td><td></td></tr>");
 
     foreach($result as $row) {
       echo("<tr><td align='center'>");
       echo($row['nomeentidade']);
-      echo("</td><td><a href=\"a2.php?mode=delete&type=entidade&nomeentidade={$row['nomeentidade']}\">delete</a></td></tr>\n");
+      echo("</td><td><a href=\"a2.php?mode=delete&type=entidade&nomeentidade={$row['nomeentidade']}\">Delete</a></td></tr>\n");
     }
     echo("</table>");
     echo("<br><br>");
-    echo("<form align='center' action='a2.php' method='post'>");
-    echo("<input type='hidden' name='mode' value='add'/>");
-    echo("<input type='hidden' name='type' value='entidade'/>");
-    echo("<input type='text' name='nomeentidade' placeholder='Nome entidade'/><br><br>");
-    echo("<button class='btn btn-info' type='submit' value='submit'>Sumit</button>");
-    echo("</form><br><br>");
 
     $result = null;
     $db = null;
 } catch (PDOException $e) {
-  echo("<p>ERROR: {$e->getMessage()}</p>");
+  echo("<div align='center'><br><p>ERROR: {$e->getMessage()}</p>");
+  echo("<a href='a2.php'><button class='btn btn-dark' style='background: #000000 !important;color: #ffffff' type='button'>Tente outra vez</button></a></div>");
 }
 ?>
   </body>
